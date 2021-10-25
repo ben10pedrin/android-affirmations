8c702194154ff116018cae6481dfb129ba6553f5
ben10pedrin <pedro123ben10@gmail.com>
2021-10-24 23:30:44 -0600

    first commit

---
+++ app/src/main/java/com/pedrom/affirmations/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.pedrom.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.pedrom.affirmations.adapter.ItemAdapter
+import com.pedrom.affirmations.data.Datasource
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val myDataset = Datasource().loadAffirmations()
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+        recyclerView.setHasFixedSize(true)
+    }
+}
